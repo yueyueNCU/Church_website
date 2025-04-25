2030b327b68f60f2e5049e0cf10d4e9a382c7b65
yueyueNCU <161934925+yueyueNCU@users.noreply.github.com>
2025-04-25 17:01:25 +0800

    configure spring boot

---
+++ src/main/java/org/kangning/church/ChurchWebApplication.java
@@ -0,0 +1,11 @@
+package org.kangning.church;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchWebApplication.class, args);
+    }
+}
