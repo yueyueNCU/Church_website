7af708f56002796b189f140123f5f8dde64317d4
yueyueNCU <161934925+yueyueNCU@users.noreply.github.com>
2025-04-26 11:56:18 +0800

    add mockito & add Service test

---
+++ src/test/java/org/kangning/church/auth/application/service/LoginServiceTest.java
@@ -0,0 +1,50 @@
+package org.kangning.church.auth.application.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kangning.church.auth.application.port.in.login.dto.LoginRequest;
+import org.kangning.church.auth.application.port.out.JwtProviderPort;
+import org.kangning.church.auth.application.port.out.UserRepositoryPort;
+import org.kangning.church.auth.domain.Role;
+import org.kangning.church.auth.domain.User;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class LoginServiceTest {
+    @Mock
+    private UserRepositoryPort userRepository;
+    @Mock
+    private JwtProviderPort jwtProvider;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private LoginService loginService;
+
+    @Test
+    void login_success_should_return_token() {
+        User mockUser = new User("john", "encoded-password", List.of(Role.LEADER));
+        when(userRepository.findByUsername("john")).thenReturn(Optional.of(mockUser));
+        when(passwordEncoder.matches("123456", "encoded-password")).thenReturn(true);
+        when(jwtProvider.generateToken(eq("john"), any())).thenReturn("mock-token");
+
+        LoginRequest request = new LoginRequest("john", "123456");
+
+        // Act
+        var response = loginService.login(request);
+
+        // Assert
+        assertEquals("mock-token", response.token());
+    }
+}
